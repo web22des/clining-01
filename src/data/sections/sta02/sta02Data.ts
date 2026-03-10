e403687b104c35c63d424cb7036e5ef15a82288a
web22des <web22designer@yandex.ru>
2026-03-10 11:48:26 +0300

    docs: add new template Sv09, add new section Sta02

---
+++ src/data/sections/sta02/sta02Data.ts
@@ -0,0 +1,7 @@
+// src/data/sections/sta02/sta02Data.ts
+import type { PropsSv09 } from "@components/templates/sv09/Sv09.astro";
+
+export const sv09Data: PropsSv09 = {
+    title: "Обращайтесь - наведем чистоту",
+    btntext: "Подробнее",
+};
