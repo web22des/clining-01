3af3c9e2a2535d70c2e6bc4bb66c0bb971cc0e0b
web22des <web22designer@yandex.ru>
2026-03-11 12:26:24 +0300

    docs: add new Hv01 and Hero

---
+++ src/data/sections/hero/heroData.ts
@@ -0,0 +1,19 @@
+// src/data/sections/hero/heroData.ts
+
+import type { Hv01Props } from "@components/templates/hv01/Hv01.astro";
+
+export const heroData: Hv01Props = {
+    heroTitle: "КЛИНИНГОВЫЕ УСЛУГИ",
+    heroSubtitle: "Для дома и бизнеса",
+
+    listSity: [
+        { text: "Москва" },
+        { text: "Воронеж" },
+        { text: "Екатеринбург" },
+        { text: "Ставрополь " },
+        { text: "Самара" },
+        { text: "Сочи" },
+        { text: "Саратов" },
+        { text: "Калининград" },
+    ],
+};
