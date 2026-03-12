6d821082e0e7e6c5a5880c73009c147a12ff1d49
web22des <web22designer@yandex.ru>
2026-03-12 12:20:47 +0300

    docs:add new covponents Tv01 and Cv100

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,8 @@
 // Валидация данных
 export { validateUniqueIds } from "./validate-data";
 
+export { initTabs } from "@utils/tv01";
+
 // Будущие утилиты (пример)
 // export { formatDate } from './formatDate';
 // export { slugify } from './slugify';
